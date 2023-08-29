b463d1ca77c028976000b5bb9662acfb35821de6
Tochukwu <nzewitochukwu26@gmail.com>
2023-08-29 23:49:57 +0100

    Implement buttons

--- src/components/Meal.js
+++ src/components/Meal.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ArrowSmRightIcon } from '@heroicons/react/outline';
 
 
 const Meal = ({ mealData }) => {
+    const [foods, setFoods] = useState(mealData)
+    const fillterCat = (category) => {
+        setFoods(
+            mealData.filter(item => {
+                return item.category === category
+            })
+        )
+    }
     return (
         <div className='max-w-[1520px] m-auto px-4 py-12'>
             <h1 className='text-orange-500 font-bold text-2xl text-center py-2'>
@@ -10,14 +18,22 @@ const Meal = ({ mealData }) => {
             </h1>
             <div className='flex flex-col lg:flex-row justify-center'>
                 <div className='flex justify-center md:justify-center'>
-                    <button className='m-1 border-orange-700 text-white hover:bg-white bg-orange-700 hover:text-orange-700 hover:border-orange-700'>All</button>
-                    <button className='m-1 border-orange-700 text-white hover:bg-white bg-orange-700 hover:text-orange-700 hover:border-orange-700'>Pizza</button>
-                    <button className='m-1 border-orange-700 text-white hover:bg-white bg-orange-700 hover:text-orange-700 hover:border-orange-700'>Chicken</button>
-                    <button className='m-1 border-orange-700 text-white hover:bg-white bg-orange-700 hover:text-orange-700 hover:border-orange-700'>Salad</button>
+                    <button
+                        onClick={() => setFoods(mealData)}
+                        className='m-1 border-orange-700 text-white hover:bg-white bg-orange-700 hover:text-orange-700 hover:border-orange-700'>All</button>
+                    <button
+                        onClick={() => fillterCat('pizza')}
+                        className='m-1 border-orange-700 text-white hover:bg-white bg-orange-700 hover:text-orange-700 hover:border-orange-700'>Pizza</button>
+                    <button
+                        onClick={() => fillterCat('chicken')}
+                        className='m-1 border-orange-700 text-white hover:bg-white bg-orange-700 hover:text-orange-700 hover:border-orange-700'>Chicken</button>
+                    <button
+                        onClick={() => fillterCat('salad')}
+                        className='m-1 border-orange-700 text-white hover:bg-white bg-orange-700 hover:text-orange-700 hover:border-orange-700'>Salad</button>
                 </div>
             </div>
             <div className='grid md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-4 gap-6 py-4'>
-                {mealData.map((item) => (
+                {foods.map((item) => (
                     <div key={item.id} className='border-none hover:scale-105 duration-300'>
                         <img src={item.image} alt={item.name}
                             className='w-full h-[200px] object-cover rounded-lg'
