373ed5627c396db39d268b5a1b50d552db47bc22
Tochukwu <nzewitochukwu26@gmail.com>
2023-08-29 23:24:14 +0100

    Implement hover buttons and styles

--- src/components/Meal.js
+++ src/components/Meal.js
@@ -8,6 +8,14 @@ const Meal = ({ mealData }) => {
             <h1 className='text-orange-500 font-bold text-2xl text-center py-2'>
                 Our Meal
             </h1>
+            <div className='flex flex-col lg:flex-row justify-center'>
+                <div className='flex justify-center md:justify-center'>
+                    <button className='m-1 border-orange-700 text-white hover:bg-white bg-orange-700 hover:text-orange-700 hover:border-orange-700'>All</button>
+                    <button className='m-1 border-orange-700 text-white hover:bg-white bg-orange-700 hover:text-orange-700 hover:border-orange-700'>Pizza</button>
+                    <button className='m-1 border-orange-700 text-white hover:bg-white bg-orange-700 hover:text-orange-700 hover:border-orange-700'>Chicken</button>
+                    <button className='m-1 border-orange-700 text-white hover:bg-white bg-orange-700 hover:text-orange-700 hover:border-orange-700'>Salad</button>
+                </div>
+            </div>
             <div className='grid md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-4 gap-6 py-4'>
                 {mealData.map((item) => (
                     <div key={item.id} className='border-none hover:scale-105 duration-300'>
